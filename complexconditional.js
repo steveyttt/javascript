f787e799f1a59e2fffa99ca81775f1ec18d137b8
steven.tyson <steven.tyson@williahill.com.au>
2017-11-21 20:04:03 +1100

    added a new complex conditional loop

--- complexconditional.js
+++ complexconditional.js
@@ -31,3 +31,22 @@ for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
   numSheep *= 4;
   console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
 }
+
+
+var totalGen = 19;
+var totalMW = 0;
+
+for (var currentGen = 1 ; currentGen <= totalGen ; currentGen ++) {
+
+  if ( currentGen % 2 == 0 && currentGen <= 4 ) {
+    totalMW += 62
+    console.log("Generator #" + currentGen + " is on, adding 62 MW, for a total of " + totalMW + " MW!")
+  }
+  else if ( currentGen % 2 == 0 && currentGen >= 5 ) {
+    totalMW += 124
+    console.log("Generator #" + currentGen + " is on, adding 124 MW, for a total of " + totalMW + " MW!")
+  }
+  else {
+    console.log("Generator #" + currentGen + " is off.")
+  }
+}
