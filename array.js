1328bd4f28c76ad32350d25dcfbfa168f5c2fde6
steven.tyson <steven.tyson@williahill.com.au>
2017-11-24 16:37:25 +1100

    added arrays

---
+++ array.js
@@ -0,0 +1,28 @@
+//arrays can store string, values, variables and other arrays.
+//Nested arrays are here
+var passengers = ["Steve", "Bob", "Frank"]
+var array1 = ["Steve", "Bob", 2, "Frank"]
+var array2 = ["me", 2, array1]
+var array3 = [array1, array2];
+
+console.log(array3)
+console.log(array3[1])
+console.log(array3[1][0])
+
+console.log(passengers[1]);
+
+passengers[2] = "Frank22";
+
+console.log(passengers[2]);
+
+console.log(passengers.length);
+
+//The pop method will remove the last item from an array and return it's value
+console.log(passengers.pop());
+
+console.log(passengers)
+
+//The push method will push a new value into the array
+passengers.push("Flamingo");
+
+console.log(passengers)
