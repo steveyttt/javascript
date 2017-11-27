69fe6bc40f0aa3e1a1b1626966bf09868b673be0
steven.tyson <steven.tyson@williahill.com.au>
2017-11-27 18:29:27 +1100

    added hoisting info

---
+++ hoisting.js
@@ -0,0 +1,9 @@
+//ensure every piece of code only runs when it needs to
+
+//functional expressions (Var functions) are never hoisted, they are treated as assignements
+//functional expressions are expressions stored as var's
+//Declared functions (traditional functions) are hoisted straight to the top
+
+
+//keep functional expressions and var's above the code so they get assigned befoer running
+//Use declared functions (traditional functions), these will be examined and stored in memory
