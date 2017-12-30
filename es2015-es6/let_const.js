90a72c1ccd66cd02f0e06f89efe8fc14f79a6a6c
steven.tyson <steven.tyson@williahill.com.au>
2017-12-30 13:56:17 +1100

    added const

--- es2015-es6/let_const.js
+++ es2015-es6/let_const.js
@@ -1,11 +1,18 @@
 //with var's they are HOISTED so they are moved to the top of the function
 //This means a var is accessible from the whole function
 //let is accessible only from the block is resides in
+
+//const allows us to create a read only constant
+//constants cannot be reassigned - so assign them values that should not be changed
+//They are block scoped so not hoisted!
+//constants MUST have a value when created
+
 function loadProfiles (userNames) {
-    if (userNames.length > 3){
+    const MAX_USERS = 3;
+    if (userNames.length > MAX_USERS){
         let loadingMessage = "This may take a while...";
         console.log(loadingMessage);
-        console.log(flashMessage);
+        //console.log(flashMessage);
     }
     else {
         let flashMessage = "Load Profiles";
@@ -16,4 +23,4 @@ function loadProfiles (userNames) {
     //console.log(flashMessage); // This causes an error - let stays within the block it is assigned and connot be accessed outside the else block
 }
 
-loadProfiles(["Rita", "Jim", "Frank"]);
+loadProfiles(["Rita", "Jim", "Frank", "Jason"]);
